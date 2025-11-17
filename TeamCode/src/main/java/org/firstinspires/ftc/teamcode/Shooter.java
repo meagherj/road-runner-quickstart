b9727250b6e4033e3a65fcf00549c753e520fc92
MeagherJ <meagherj@greenhill.org>
2025-11-17 13:02:02 -0600

    Make concept for flywheel and distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -1,22 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Shooter {
-    DcMotor shooter;
-    DcMotor intake;
+    DcMotorEx shooter;
     Servo servo;
     String state = "nothing";
     Telemetry telemetry;
     double position;
     double shooterSpeed;
-    public Shooter(DcMotor intake, DcMotor shooter, Servo servo, Telemetry telemetry){
-        this.intake = intake;
-        this.shooter = shooter;
-        this.servo = servo;
+
+    double range;
+    public Shooter(HardwareMap hw, Telemetry telemetry){
+        this.shooter = hw.get(DcMotorEx.class, "shooter");
+        this.servo = hw.get(Servo.class, "left_hand");
         this.telemetry = telemetry;
     }
     public void update(){
@@ -36,14 +37,27 @@ public class Shooter {
                 break;
         }
         servo.setPosition(position);
-        shooter.setPower(shooterSpeed);
-        intake.setPower(shooterSpeed);
-        telemetry.addData("org.firstinspires.ftc.teamcode.Shooter Speed", shooterSpeed);
+        shooter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shooter.setVelocity(shooterSpeed);
+        telemetry.addData("Shooter Speed", shooterSpeed);
         telemetry.addData("servoPos", position);
-
     }
     public void spinClose(){ state = "spinup"; shooterSpeed = .58;}
     public void spinFar(){state="spinup"; shooterSpeed = 0.5;}
     public void drop(){state = "shoot";}
     public void stop(){state="stop";}
+
+    public void setRange(double range){
+        // from range get shooter speed as Ticks Per Second
+        double gears = 3; // 1/3 gears to speed it up
+        double ticks_rotation = 145.1; // shaft tics per rev on 1150 motor
+        double radius = .0043; // 96mm/2=43mm then convert to meters
+        double twoPiR = 2*Math.PI*radius;
+        double gravity = 9.8; // in meters/s squared
+        double top = Math.sqrt(range * gravity);
+        double spins = top/twoPiR;
+        double ticksPerSec = spins/gears*ticks_rotation;
+        telemetry.addData("TicksPerSecond", ticksPerSec);
+        shooterSpeed = ticksPerSec;
+    }
 }
